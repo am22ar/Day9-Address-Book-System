359a8f5ecdf8c47ac515447cfdd611964f191c49
am22ar <amarpatil12222@gmail.com>
2022-10-06 12:46:52 +0530

    Added: Welcome to Address Book system Program

---
+++ src/com/book/addressbook/AddressBookMain.java
@@ -0,0 +1,12 @@
+package com.book.addressbook;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Address Book system Program ");
+		ContactPerson cp = new ContactPerson();
+		System.out.println(cp.toString());
+
+	}
+
+}
